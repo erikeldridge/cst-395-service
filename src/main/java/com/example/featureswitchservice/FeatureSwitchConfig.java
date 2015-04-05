14037fb970251d384c58485196e30290a70d0742
Erik Eldridge <erik@example.com>
2015-04-05 22:58:36 +0000

    Return feature config as json

--- src/main/java/com/example/featureswitchservice/FeatureSwitchConfig.java
+++ src/main/java/com/example/featureswitchservice/FeatureSwitchConfig.java
@@ -1,25 +1,30 @@
 package com.example.featureswitchservice;
 
+import com.esotericsoftware.yamlbeans.YamlException;
+import com.example.featureselector.FeatureSelector;
+import com.example.featureselector.Input;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
 @Path("feature_switch_config")
 public class FeatureSwitchConfig {
+    @QueryParam("id") String id;
+    @QueryParam("os") String os;
+    @QueryParam("version") String version;
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Hello, Heroku!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Boolean> get() throws FileNotFoundException, YamlException {
+      Input input = FeatureSelector.requireInput(new String[]{id, os, version});
+      Map<String, Map<String, String>> features = FeatureSelector.createFeatureMap();
+      FeatureSelector selector = new FeatureSelector(features);
+      Map<String, Boolean> selected = selector.select(input);
+      return selected;
     }
 }
--- src/test/java/com/example/featureswitchservice/FeatureSwitchConfigTest.java
+++ src/test/java/com/example/featureswitchservice/FeatureSwitchConfigTest.java
@@ -2,26 +2,44 @@ package com.example.featureswitchservice;
 
 import javax.ws.rs.core.Application;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class FeatureSwitchConfigTest extends JerseyTest {
 
     @Override
     protected Application configure() {
-        return new ResourceConfig(FeatureSwitchConfig.class);
+      enable(TestProperties.LOG_TRAFFIC);
+      enable(TestProperties.DUMP_ENTITY);
+      return new ResourceConfig(FeatureSwitchConfig.class);
     }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
     @Test
-    public void testGetIt() {
-        final String responseMsg = target().path("feature_switch_config").request().get(String.class);
-
-        assertEquals("Hello, Heroku!", responseMsg);
+    public void testGet() throws IOException {
+      final String json = target()
+          .path("feature_switch_config")
+          .queryParam("id", "123")
+          .queryParam("os", "android")
+          .queryParam("version", "2.3")
+          .request()
+          .get(String.class);
+      HashMap<String, Boolean> expected = new HashMap<>();
+      expected.put("feature_a", true);
+      expected.put("feature_b", false);
+      expected.put("feature_c", true);
+      ObjectMapper mapper = new ObjectMapper();
+      Map<String, Boolean> actual = mapper.readValue(json, Map.class);
+      assertEquals(expected, actual);
     }
 }
